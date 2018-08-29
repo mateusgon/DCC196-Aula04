bf3c3f8d6da4404c3f9f12b1ef48d3a8f9408e6d
Mateus Gonçalo <mateusgon57@gmail.com>
2018-08-29 20:35:31 -0300

    Adiciona notificações para o estado da ProfessorActivity

--- app/src/main/java/br/ufjf/dcc196/exemplo02/ProfessorActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo02/ProfessorActivity.java
@@ -3,6 +3,7 @@ package br.ufjf.dcc196.exemplo02;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfessorActivity extends AppCompatActivity {
 
@@ -19,5 +20,42 @@ public class ProfessorActivity extends AppCompatActivity {
             String nome = extras.getString(MainActivity.PESSOA_NOME);
             txtProfMensagem.setText("Olá prof. " + nome);
         }
+        Toast.makeText(getApplicationContext(),"prof onCreate()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(),"prof onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(),"prof onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"prof onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(),"prof onStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),"prof onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"prof onDestroy()", Toast.LENGTH_SHORT).show();
     }
 }
